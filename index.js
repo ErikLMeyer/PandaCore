9befdc416353a0820ebeaa33d0d65bb5cd1f91c9
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-24 10:20:28 -0500

    Installed express-handlebars

--- index.js
+++ index.js
@@ -4,10 +4,16 @@
 */
 
 const express = require('express');
+const expressHandelbars = require('express-handlebars');
 const app = express();
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/'));
+
+app.engine('handlebars', expressHandelbars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars');
 
 app.get('/', (req,res) => res.render('index'));
 
