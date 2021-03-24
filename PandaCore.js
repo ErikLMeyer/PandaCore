3779aaae5958fa16849d354c952c4174fb08ff43
ErikLMeyer <erikmeyer@lewisu.edu>
2021-03-24 14:47:52 -0500

    Message

--- PandaCore.js
+++ PandaCore.js
@@ -8,7 +8,7 @@ const expressHandlebars = require('express-handlebars');
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/PandaCore'));
+app.use(express.static(__dirname + '/public'));
 
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main',
